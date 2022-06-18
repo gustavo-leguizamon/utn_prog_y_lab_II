9896b995e41b57ba348b97d1cdb2c7e7ae0af055
Gustavo Leguizamón <gustavoleguizamn@gmail.com>
2022-06-17 21:20:43 -0300

    TP3 - archivos

---
+++ TP3/Archivos/ArchivoJson.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class ArchivoJson<T> : Archivo, IArchivo<T>
+      where T : class
+    {
+        protected override string Extension => ".json";
+
+        public void Guardar(string ruta, T contenido)
+        {
+            string jsonString = JsonSerializer.Serialize(contenido);
+            File.WriteAllText(ruta, jsonString);
+        }
+
+        public T Leer(string ruta)
+        {
+            ValidarSiExisteArchivo(ruta);
+            ValidarExtension(ruta);
+            string jsonString = File.ReadAllText(ruta);
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+    }
+}
---
+++ TP3/Archivos/ExtensionIncorrectaException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class ExtensionIncorrectaException : Exception
+    {
+        public ExtensionIncorrectaException(string mensaje)
+            : base(mensaje)
+        {
+
+        }
+    }
+}
